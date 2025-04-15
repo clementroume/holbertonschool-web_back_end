bab4d6e6c76612a87e70a6373155e21fc615636a
Clément Roume <clement.roume@hotmail.com>
2025-04-15 10:00:22 +0200

    0. Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class that inherits from BaseCaching
+    """
+    def __init__(self):
+        """
+        Initialize the BasicCache instance
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item from the cache
+        """
+        if key:
+            return self.cache_data.get(key)
+        return None
