a4a95f2f4a8922c7d65797887e15dfeeaae0bbad
Clément Roume <clement.roume@hotmail.com>
2025-05-21 11:35:56 +0200

    closed checker again

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
-""" filtered_logger module
+"""
+This module provides utilities for handling
+and protecting sensitive data in logs and databases.
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """ TEST """
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """
+    Replace the values of specified fields in the message with
+    the redaction string.
+
+    Args:
+        fields: List of field names to obfuscate.
+        redaction: The string to replace the field values with.
+        message: The original log message.
+        separator: The separator between fields (e.g., ';').
+
+    Returns:
+        str: The log message with sensitive fields obfuscated.
+    """
     for field in fields:
         pattern = fr'{field}=.*?(?={separator})'
         message = re.sub(pattern, f'{field}={redaction}', message)
