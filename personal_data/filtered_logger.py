90e8f8e066f1885d3e278d4e6b45f0d487d29f12
Clément Roume <clement.roume@hotmail.com>
2025-05-21 11:31:45 +0200

    checker encore et toujours

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,19 +5,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """
-    Replace the values of specified fields in the message with
-    the redaction string.
-
-    Args:
-        fields: List of field names to obfuscate.
-        redaction: The string to replace the field values with.
-        message: The original log message.
-        separator: The separator between fields (e.g., ';').
-
-    Returns:
-        str: The log message with sensitive fields obfuscated.
-    """
+    """ TEST """
     for field in fields:
         pattern = fr'{field}=.*?(?={separator})'
         message = re.sub(pattern, f'{field}={redaction}', message)
