97b5ced661bad048b51b4d0847b703ac529fcb88
Clément Roume <clement.roume@hotmail.com>
2025-06-06 11:08:22 +0200

    5. Request validation!

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -25,7 +25,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Return the Authorization header from the request."""
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Return the current user from the request."""
