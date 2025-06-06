6f12c3b8eff7167169ecb8b3f508f90c47c883da
Clément Roume <clement.roume@hotmail.com>
2025-06-06 11:32:20 +0200

    Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,19 @@ class BasicAuth(Auth):
     This class inherits from Auth and will be used for handling
     HTTP Basic Authentication.
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header.
+
+        Args:
+            authorization_header (str): The Authorization header.
+
+        Returns:
+            str: The Base64 part of the header, or None if not valid.
+        """
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        if " " in authorization_header:
+            return authorization_header.split(" ", 1)[1]
+        return None
