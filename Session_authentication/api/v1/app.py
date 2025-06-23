71db7df2c03b3ef133228e231f8e5ac2f8070d6f
Clément Roume <clement.roume@hotmail.com>
2025-06-23 11:47:26 +0200

    5. Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -59,7 +59,8 @@ def before_request_handler():
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
@@ -69,6 +70,12 @@ def before_request_handler():
 
     if auth.current_user(request) is None:
         abort(403)
+
+    if (
+        auth.authorization_header(request) is None and
+        auth.session_cookie(request) is None
+    ):
+        abort(401)
 
     request.current_user = auth.current_user(request)
 
