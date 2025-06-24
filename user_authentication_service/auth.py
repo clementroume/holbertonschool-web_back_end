d468ce6f8f33bfcb39618141b215b42a63ed6d95
Clément Roume <clement.roume@hotmail.com>
2025-06-24 11:33:48 +0200

    8. Credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -29,3 +29,14 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate a user's login credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+            return (
+                hashpw(password.encode('utf-8'), user.hashed_password)
+                == user.hashed_password
+            )
+        except NoResultFound:
+            return False
