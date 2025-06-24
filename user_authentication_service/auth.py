321521467ff3be29787fff6b8ffcdd8c8cc9a94d
Clément Roume <clement.roume@hotmail.com>
2025-06-24 11:41:41 +0200

    8. Credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -27,8 +27,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exist")
         except NoResultFound:
-            hashed_password = _hash_password(password)
-            return self._db.add_user(email, hashed_password)
+            return self._db.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validate a user's login credentials."""
