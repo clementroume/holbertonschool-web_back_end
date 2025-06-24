033b60ab03493b3ebe2825a268d00be72677bcfb
Clément Roume <clement.roume@hotmail.com>
2025-06-24 12:53:10 +0200

    13. Destroy session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -60,3 +60,11 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a user's session by user ID."""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except NoResultFound:
+            raise ValueError("User not found")
