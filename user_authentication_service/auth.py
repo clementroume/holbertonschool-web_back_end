39a8a989d27ed0e68b37ab41b9b9520149edfef6
Clément Roume <clement.roume@hotmail.com>
2025-06-24 10:51:30 +0200

    4. Hash password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """Database module for user authentication service."""
 
+from bcrypt import checkpw, hashpw, gensalt
+
 
 def _hash_password(password: str) -> str:
     """Hash a password using bcrypt."""
-    import bcrypt
-
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed.decode('utf-8')
+    return hashpw(password.encode('utf-8'), gensalt())
