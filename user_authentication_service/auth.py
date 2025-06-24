026b62964762ad642d5097ff670a452a0e2cb5a8
Clément Roume <clement.roume@hotmail.com>
2025-06-24 14:15:25 +0200

    18. Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -74,3 +74,13 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError("No user found with this email")
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a user's password using a reset token."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(
+                user.id, hashed_password=hashed_password, reset_token=None)
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
