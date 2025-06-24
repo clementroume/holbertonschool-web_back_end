1dd7c5bbf47620b2b93c08871717bab1d7612011
Clément Roume <clement.roume@hotmail.com>
2025-06-24 11:28:17 +0200

    7. Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 """Database module for user authentication service."""
 
-from flask import Flask, Response, jsonify
+from flask import Flask, Response, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def index() -> Response:
     """Index route for the API."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def user() -> Response:
+    """Register a new user."""
+    email: str = request.form.get('email')
+    password: str = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
