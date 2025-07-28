6e658cae096f20d9e55128a4c467abb18ed6ad32
Clément Roume <clement.roume@hotmail.com>
2025-07-28 18:07:29 +0200

    3. Spies

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -7,6 +7,7 @@ const Utils = require('./utils.js');
  */
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
 };
 
 module.exports = sendPaymentRequestToApi;
